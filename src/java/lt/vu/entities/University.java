159d51b5d80874bc7a5d2c76c4f1b1219085860e
vizitas <vizitas7@gmail.com>
2016-03-08 09:16:07 +0200

    added entities

---
+++ src/java/lt/vu/entities/University.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.vu.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author donatas
+ */
+@Entity
+@Table(name = "UNIVERSITY")
+@NamedQueries({
+    @NamedQuery(name = "University.findAll", query = "SELECT u FROM University u"),
+    @NamedQuery(name = "University.findById", query = "SELECT u FROM University u WHERE u.id = :id"),
+    @NamedQuery(name = "University.findByTitle", query = "SELECT u FROM University u WHERE u.title = :title"),
+    @NamedQuery(name = "University.findByOptLockVersion", query = "SELECT u FROM University u WHERE u.optLockVersion = :optLockVersion")})
+public class University implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 50)
+    @Column(name = "TITLE")
+    private String title;
+    @Column(name = "OPT_LOCK_VERSION")
+    @Version
+    private Integer optLockVersion;
+    @OneToMany(mappedBy = "universityId")
+    private List<Student> studentList;
+
+    public University() {
+    }
+
+    public University(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getOptLockVersion() {
+        return optLockVersion;
+    }
+
+    public void setOptLockVersion(Integer optLockVersion) {
+        this.optLockVersion = optLockVersion;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (title != null ? title.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof University)) {
+            return false;
+        }
+        University other = (University) object;
+        if ((this.title == null && other.title != null) || 
+                (this.title != null && !this.title.equals(other.title))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "lt.vu.entities.University[ id=" + id + " ]";
+    }
+    
+}
